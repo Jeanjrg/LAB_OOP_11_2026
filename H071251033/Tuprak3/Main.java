3cb3b30d0bffc7b7331c050e08cad60d271d49a4
nrfdhillahdills <nrfdhillahfauziah@gmail.com>
2026-04-16 00:56:36 +0800

    Tugas Praktikum 3

---
+++ H071251033/Tuprak3/Main.java
@@ -0,0 +1,20 @@
+package H071251033.Tuprak3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DompetDigital dompet = new DompetDigital("USR001", "123456");
+
+        System.out.println("ID Nasabah: " + dompet.getIdNasabah());
+        dompet.ubahPin("000000", "654321");
+        dompet.ubahPin("123456", "654321");
+
+        dompet.setor(500000);
+        dompet.setor(-100);
+
+        dompet.tarik(100000, "123456");
+        dompet.tarik(100000, "654321");
+
+        System.out.println("Saldo akhir: " + dompet.getSaldo());
+    }
+}
